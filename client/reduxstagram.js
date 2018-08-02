d8cb5d45ec5ed96965935611c465c3b4945a1485
sandond77 <sandu@ucdavis.edu>
2018-08-02 14:34:32 -0700

    Started video 2. Test code functional

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,11 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//Import CSS
+import css from './styles/style.styl';
+
+render(
+	<p>hello</p>, 
+	document.getElementById('root')
+);
