f2899f5fc84ac652d9fe57773463d81f707be942
sandond77 <sandu@ucdavis.edu>
2018-08-03 15:21:32 -0700

    completed store.js file

--- client/store.js
+++ client/store.js
@@ -1,3 +1,21 @@
 import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
+
+//import root reducer
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+//creating default data;
+
+const defaultState = {
+	posts,
+	comments
+}
+
+export default const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
