47d80c393909d105d996d1da167f40d802df39a5
sandond77 <sandu@ucdavis.edu>
2018-08-03 15:14:23 -0700

    started tutorial #5; created store.js file?

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,53 +1,27 @@
-// import React from 'react';
-// import { render } from 'react-dom';
-
-// //Import CSS
-// import css from './styles/style.styl';
-
-// //import components
-// import Main from './components/Main';
-// import Single from './components/Single';
-// import PhotoGrid from './components/PhotoGrid';
-
-// //react router
-// import { Router, Route, IndexRoute, BrowserHistory } from 'react-router';
-
-// const router = (
-// 	<Router history={browserHistory}>
-// 		<Route path="/" component={Main}>
-// 			<IndexRoute component={PhotoGrid}></IndexRoute>
-// 			<Route path="/view/:postId" component={Single}></Route>
-// 		</Route>
-// 	</Router>
-// )
-
-// render(
-// 	router,
-// 	document.getElementById('root')
-// );
-
 import React from 'react';
-
 import { render } from 'react-dom';
 
-// Import css
+//Import CSS
 import css from './styles/style.styl';
 
-// Import Components
+//import components
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
-// import react router deps
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+//react router
+import { Router, Route, IndexRoute, BrowserHistory } from 'react-router';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<IndexRoute component={PhotoGrid}></IndexRoute>
+			<Route path="/view/:postId" component={Single}></Route>
+		</Route>
+	</Router>
 )
 
-render(router, document.getElementById('root'));
+render(
+	router,
+	document.getElementById('root')
+);
---
+++ client/store.js
@@ -0,0 +1,3 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
