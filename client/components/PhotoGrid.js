9e3b0cf757bfbd93dbd34dcd412ac1b67705c965
sandond77 <sandu@ucdavis.edu>
2018-08-07 15:08:28 -0700

    connected photo.js component to main app

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
 	render(){
 		return (
 			<div className="photo-grid">
-				<pre>
-				{JSON.stringify(this.props.posts,null,'')}
-				</pre>
+				{this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i}/>)}
 			</div>
 		)
 	}
