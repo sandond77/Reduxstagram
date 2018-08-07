ef039a2c92fbc7f642131f95c8422895f86a8098
sandond77 <sandu@ucdavis.edu>
2018-08-06 21:37:06 -0700

    added provided to reduxstagram.js file

--- client/store.js
+++ client/store.js
@@ -15,7 +15,8 @@ const defaultState = {
 	comments
 }
 
-export default const store = createStore(rootReducer, defaultState);
+const store = createStore(rootReducer, defaultState);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
+export default store;
