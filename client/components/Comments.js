b8a6369e9345ff5f5fd6b3a5f11e4a2bdabf085f
sandond77 <sandu@ucdavis.edu>
2018-08-07 22:04:24 -0700

    added dispatching of add comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,7 +7,7 @@ const Comment = React.createClass({
 				<p>
 					<strong>{comment.user}</strong>
 					{comment.text}
-					<button className="remove-comment">&times;</button>
+					<button className="remove-comment"> &times; </button>
 				</p>
 			</div>
 		)
@@ -19,12 +19,13 @@ const Comment = React.createClass({
 		const author = this.refs.author.value;
 		const comment = this.refs.comment.value;
 		this.props.addComment(postId, author, comment);
+		this.refs.commentForm.reset();
 	},
 
 	render(){
 		return(
 			<div className="comment">
-				{this.props.postComments.map(this.renderComment)};
+				{this.props.postComments.map(this.renderComment)}
 				<form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
 					<input type="text" ref="author" placeholder="author" />
 					<input type="text" ref="comment" placeholder="comment" />
